c10419737f808c6bbd1988b35fb0fd92e0526c2f
Francis0023 <150805817+Francis0023@users.noreply.github.com>
2023-11-21 07:56:33 -0500

    v

--- Ejercicio1_martes.java
+++ Ejercicio1_martes.java
@@ -1,3 +1,24 @@
 public class Ejercicio1_martes {
 
+ public class MiClase {
+    // Atributos de la clase
+    String atributo1;
+    int atributo2;
+    float atributo3;
+   //Consultor: Inicia la clase
+    MiClase(){}
+
+    //Metodos:
+    void metodo1(){
+
+    }
+    void metodo2(){
+
+    }
+    void metodo3(){
+
+    }
+
+}
+
 }
